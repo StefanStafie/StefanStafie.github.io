c9d35ac6a19f142df60e1b5fece0412191b788e8
unknown <stefan_stafie@yahoo.comgit config --global user.name StefanStafie>
2021-02-14 13:52:46 +0200

    upgrade

--- javascript.js
+++ javascript.js
@@ -1,37 +1,43 @@
 var a;
-function mech(a) {
-	document.getElementById("content1").style.display='none';
-	document.getElementById("content2").style.display='none';
-	document.getElementById("content3").style.display='none';
-	document.getElementById("content4").style.display='none';
-	document.getElementById("choice1").style.borderLeft = '10px solid #280028';
-	document.getElementById("choice2").style.borderLeft = '10px solid #280028';
-	document.getElementById("choice3").style.borderLeft = '10px solid #280028';
-	document.getElementById("choice4").style.borderLeft = '10px solid #280028';
-	document.getElementById("choice1").style.borderBottom = '2px solid #280028';
-	document.getElementById("choice2").style.borderBottom = '2px solid #280028';
-	document.getElementById("choice3").style.borderBottom = '2px solid #280028';
-	document.getElementById("choice4").style.borderBottom = '2px solid #280028';
 
-  if(a==1){
-  	var yourUl = document.getElementById("content1");
-	yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
-	document.getElementById("choice1").style.borderLeft = '10px solid #900000';
-	document.getElementById("choice1").style.borderBottom = '1px solid #900000';
-  }else if(a==2){
-  	var yourUl = document.getElementById("content2");
-	yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
-	document.getElementById("choice2").style.borderLeft = '10px solid #900000';
-	document.getElementById("choice2").style.borderBottom = '1px solid #900000';
-  }else if(a==3){
-  	var yourUl = document.getElementById("content3");
-	yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
-	document.getElementById("choice3").style.borderLeft = '10px solid #900000';
-	document.getElementById("choice3").style.borderBottom = '1px solid #900000';
-  } else{
-  	var yourUl = document.getElementById("content4");
-	yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
-	document.getElementById("choice4").style.borderLeft = '10px solid #900000';
-	document.getElementById("choice4").style.borderBottom = '1px solid #900000';
-  }
+function mech(a) {
+    document.getElementById("content1").style.display = 'none';
+    document.getElementById("content2").style.display = 'none';
+    document.getElementById("content3").style.display = 'none';
+    document.getElementById("content4").style.display = 'none';
+    var yourUl = document.getElementById("choice1");
+    yourUl.style.background = 'repeating-linear-gradient(#280028, black 5px)';
+
+    var yourUl = document.getElementById("choice2");
+    yourUl.style.background = 'repeating-linear-gradient(#280028, black 5px)';
+
+    var yourUl = document.getElementById("choice3");
+    yourUl.style.background = 'repeating-linear-gradient(#280028, black 5px)';
+
+    var yourUl = document.getElementById("choice4");
+    yourUl.style.background = 'repeating-linear-gradient(#280028, black 5px)';
+
+    if (a == 1) {
+        var yourUl = document.getElementById("content1");
+        yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
+        var yourUl = document.getElementById("choice1");
+        yourUl.style.background = 'repeating-linear-gradient(#280028, transparent 5px), linear-gradient(0deg, rgba(40, 0, 40, 1) 95%, black 100%)';
+
+    } else if (a == 2) {
+        var yourUl = document.getElementById("content2");
+        yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
+        var yourUl = document.getElementById("choice2");
+        yourUl.style.background = 'repeating-linear-gradient(#280028, transparent 5px), linear-gradient(0deg, rgba(40, 0, 40, 1) 95%, black 100%)';
+
+    } else if (a == 3) {
+        var yourUl = document.getElementById("content3");
+        yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
+        var yourUl = document.getElementById("choice3");
+        yourUl.style.background = 'repeating-linear-gradient(#280028, transparent 5px), linear-gradient(0deg, rgba(40, 0, 40, 1) 95%, black 100%)';
+    } else {
+        var yourUl = document.getElementById("content4");
+        yourUl.style.display = yourUl.style.display === 'none' ? '' : 'none';
+        var yourUl = document.getElementById("choice4");
+        yourUl.style.background = 'repeating-linear-gradient(#280028, transparent 5px), linear-gradient(0deg, rgba(40, 0, 40, 1) 95%, black 100%)';
+    }
 }
